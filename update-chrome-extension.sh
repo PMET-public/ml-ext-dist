24f2cfd6b2e8ee7e5ad5fcf96d63e3e05370430c
Keith Bentrup <keith@keithbentrup.com>
2021-11-19 13:29:09 -0500

    dist

--- update-chrome-extension.sh
+++ update-chrome-extension.sh
@@ -9,7 +9,7 @@ rm -rf ~/Downloads/mkto-chrome-ext.zip || :
 
 master_ver="$(curl -sS https://raw.githubusercontent.com/PMET-public/ml-ext-dist/master/manifest.json | perl -ne 's/^\s*"version"\s*:\s*"(.*)".*/\1/ and print')"
 
-curl -L -o ~/Downloads/mkto-chrome-ext.zip --create-dirs https://github.com/PMET-public/ml-ext-dist/releases/download/v${master_ver}/mkto-chrome-ext.zip
+curl -L -o ~/Downloads/mkto-chrome-ext.zip --create-dirs "https://github.com/PMET-public/ml-ext-dist/releases/download/v${master_ver}/mkto-chrome-ext.zip"
 
 # delete everything but the existing manifest (so chrome will not throw an error but the manifest will still be overwritten when the zip is expanded)
 find ~/Downloads/mkto-chrome-ext -type f ! -name 'manifest.json' -delete 2> /dev/null || :
