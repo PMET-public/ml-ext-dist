3631abb28d7366e3ff0f07c536b135f0a5e4c42c
ASHISH GUPTA <ashishgu@adobe.com>
2022-05-09 20:29:37 +0530

    dist

--- web-accessible-resources/marketo-app-admin.js
+++ web-accessible-resources/marketo-app-admin.js
@@ -1,5 +1,5 @@
 // DO NOT EDIT! All changes will be lost. This is a temporary, auto-generated file using gulp to combine javascript sources.
-window.MARKETO_EXT_VERSION = 'v5.4.24'; // version also automatically injected via gulp using manifest.json
+window.MARKETO_EXT_VERSION = 'v5.4.25'; // version also automatically injected via gulp using manifest.json
 
 console.log('Marketo App Admin > Running', MARKETO_EXT_VERSION)
 /**************************************************************************************
